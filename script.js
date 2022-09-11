76eee87145435326c7bf0047454b4c84164f7b44
Kyro <makermanu@gmail.com>
2022-09-11 11:18:32 +0200

    Site restructure

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const themeToggler = document.querySelector(".theme-toggler");
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
 const sideBtn = sideMenu.querySelectorAll("a");
+const themeToggler = document.querySelector(".theme-toggler");
 
 sideBtn.forEach((item) =>{
     item.addEventListener('click', () =>{
@@ -13,33 +13,33 @@ sideBtn.forEach((item) =>{
 })
 
 
-menuBtn.addEventListener('click', () =>{
-    sideMenu.style.display = "block";
-})
+// menuBtn.addEventListener('click', () =>{
+//     sideMenu.style.display = "block";
+// })
 
-closeBtn.addEventListener('click', () =>{
-    sideMenu.style.display = "none";
-})
+// closeBtn.addEventListener('click', () =>{
+//     sideMenu.style.display = "none";
+// })
 
 themeToggler.addEventListener('click', () =>{
-    document.body.classList.toggle('dark-theme-variables');
+    document.body.classList.toggle("dark-theme-variables");
 
-    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
-    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+    // themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    // themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 })
 
 
-Orders.forEach(order =>{
-    const tr = document.createElement('tr');
-    const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.shipping === 'Declined' ? 'danger' :
-        order.shipping === 'pending' ?
-         'warning' : 'primary'}">${order.shipping}</td>
-        <td class="primary">Details</td>
-    `;
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
-})
+// Orders.forEach(order =>{
+//     const tr = document.createElement('tr');
+//     const trContent = `
+//         <td>${order.productName}</td>
+//         <td>${order.productNumber}</td>
+//         <td>${order.paymentStatus}</td>
+//         <td class="${order.shipping === 'Declined' ? 'danger' :
+//         order.shipping === 'pending' ?
+//          'warning' : 'primary'}">${order.shipping}</td>
+//         <td class="primary">Details</td>
+//     `;
+//     tr.innerHTML = trContent;
+//     document.querySelector('table tbody').appendChild(tr);
+// })
